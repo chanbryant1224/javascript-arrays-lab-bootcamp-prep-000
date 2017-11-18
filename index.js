70f736684b4dc6d51d58a1b760b5a7128897a5f4
Wing Chak Chan <chanbryant1224@gmail.com>
2017-11-18 08:24:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){return kittens.push(name)}
 function destructivelyPrependKitten(name){return kittens.unshift(name)}
 function destructivelyRemoveLastKitten(name){return kittens.pop(name)}
+function destructivelyRemoveFirstKitten(name){return kittens.shift(name)}
